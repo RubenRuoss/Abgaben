226cec6bfd1d6a70c9e781b7f7a71a548a6d5515
RubenRuoss <92145556+RubenRuoss@users.noreply.github.com>
2021-11-10 17:01:57 +0100

    Update script_vorlage.ts

--- Augabe3/script_vorlage.ts
+++ Augabe3/script_vorlage.ts
@@ -54,17 +54,78 @@ let events: any[][] = [
 // -- HIER BITTE IHRE LÖSUNG ZUR AUFGABE 2 EINTRAGEN
 
 // Lösung a) ...
-
+console.log(`Das Array Events ist ${events.length} Einträge lang`);
 // Lösung b) ...
-
+console.log(events[0]);
+console.log(events[1]);
+console.log(events[2]);
+console.log(events[3]);
+console.log(events[4]);
+console.log(events[5]);
+console.log(events[6]);
+console.log(events[7]);
+console.log(events[8]);
+console.log(events[9]);
 // Lösung c) ...
-
+let HöchsterTicketPreis: number = 0;
+for (let i: number = 0; i < events.length; i++) {
+  if (events[i][1] > HöchsterTicketPreis) {
+    HöchsterTicketPreis = events[i][1];
+  }
+}
+console.log(HöchsterTicketPreis);
 // Lösung d) ...
-
+let Künstlername : any = "Maria Carey";
+let Vorhanden : Boolean = false; 
+for (let i = 0; i < events.length; i++) {
+  if (events[i] = Künstlername) {
+    Vorhanden = true;
+    break;
+  }
+}
+console.log;
 // Lösung e) ...
-
+var n: number;
+function factorial (n: number): number {
+  let i: number = n;
+  while ( i > 0 ) {
+    n += i * i - 1 ;
+  }
+  return n;
+} 
+console.log(factorial);
 // Lösung f) ...
-
+let durch3Teilbar: number[];
+for (let index = 0; index < 101; index++) {
+  if (index % 3 == 0) {
+    durch3Teilbar.push(index);
+  }
+}
+console.log(durch3Teilbar);
+console.log(`Es gibt ${durch3Teilbar.length} Zahlen zwischen 1 und 100 die durch drei Teilbar sind.`);
 // Lösung g) ...
+class ConcertEvent {
+  private interpret: string;
+  private price: number = 0;
 
+  constructor(interpret: string, price: number) {
+    this.interpret = interpret;
+    this.price = price;
+  }
+
+  show(): string {
+    return `Der Eintritt bei${this.interpret} kostet ${this.price}€.`;
+  }
+}
 // Lösung h) ...
+var ConcertEvents : ConcertEvent[];
+ConcertEvents.push(new ConcertEvent(`Mark Knopfler`, 10.1));
+ConcertEvents.push(new ConcertEvent("Pink Floyd", 15.9));
+ConcertEvents.push(new ConcertEvent("Metallica", 20.1));
+ConcertEvents.push(new ConcertEvent("Michael Bublé", 11.1));
+ConcertEvents.push(new ConcertEvent("Dire Straits", 12.2));
+ConcertEvents.push(new ConcertEvent("Mariah Carey", 1.1));
+ConcertEvents.push(new ConcertEvent("Cat Stevens", 12.99));
+ConcertEvents.push(new ConcertEvent("Mark Forster", 2.1));
+ConcertEvents.push(new ConcertEvent("Helene Fischer", 3.1));
+ConcertEvents.push(new ConcertEvent("Bee Gees", 25.2));
